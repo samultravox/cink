7e5a41857bd4dec6a8f10f0d8f92417e5b0330c8
samultravox <martinpsvarc@gmail.com>
2025-07-03 08:28:13 +0100

    Fix TypeScript error in clients table

--- components/clients/clients-table.tsx
+++ components/clients/clients-table.tsx
@@ -629,7 +629,7 @@ export function ClientsTable({
             <div className="flex items-center space-x-1">
               {Array.from({ length: Math.min(5, totalPages) }, (_, i) => {
                 // Calculate which page numbers to show
-                let pageNum;
+                let pageNum: number;
                 if (totalPages <= 5) {
                   pageNum = i + 1;
                 } else if (currentPage <= 3) {
